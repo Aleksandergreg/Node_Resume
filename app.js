f00ff5a1a041612f1b707327098204be2474dc6a
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-13 09:17:56 +0100

    made an express route for parent /javascript endpoint

--- app.js
+++ app.js
@@ -16,5 +16,9 @@ app.get("/git", (req, res) => {
     res.sendFile(path.resolve('public/git/git.html'));
 });
 
+app.get("/javascript", (req, res) =>{
+    res.sendFile(path.resolve('public/javascript/javascript.html'))
+});
+
 const PORT = Number(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));

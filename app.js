ace79bae55c918c2bba58a5b3ba2d554b9e44bcf
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-13 08:25:28 +0100

    Added the git.html page edited the route in frontpage.html and mate a server route for it in app.js

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.get("/", (req, res) => {
     res.sendFile(path.resolve('public/frontpage/frontpage.html'));
 });
 
+app.get("/git", (req, res) => {
+    res.sendFile(path.resolve('public/git/git.html'));
+});
+
 const PORT = Number(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));

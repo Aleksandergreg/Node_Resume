8d58ddfe384303b1b7ff59ba67f8c27976e9ec72
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-13 09:09:46 +0100

    Styled the sidebar and git page using tailwind. Made a sidebar.js asset so don't have to copy in the navbar to each single html site

---
+++ public/assets/js/sidebar.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const sidebarContainer = document.getElementById('sidebar-container');
+  sidebarContainer.innerHTML = `
+    <nav class="relative w-64 h-full bg-blue-600 text-white transform -skew-y-6">
+      <div class="absolute inset-0 transform skew-y-6">
+        <ul class="py-8 space-y-4">
+          <li>
+            <a href="/" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/git" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+              Git
+            </a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+              Node.js
+            </a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+              Express.js
+            </a>
+          </li>
+          <li>
+            <a href="#" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+              Rest API
+            </a>
+          </li>
+          <li>
+            <div class="group">
+              <a href="#" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+                JavaScript
+              </a>
+              <ul class="hidden group-hover:block bg-blue-700 mt-1 space-y-2 rounded">
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
+                    Datatypes in JavaScript
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
+                    Scopes in JavaScript
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
+                    Functions in JavaScript
+                  </a>
+                </li>
+                <li>
+                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
+                    Loops in JavaScript
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  `;
+});

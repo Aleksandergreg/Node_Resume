21a52b420972bb216e2e83c428d69afaa255aed6
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-13 08:16:21 +0100

    Added the navbar with the intended content on frontpage.html, some simple styling and the route in app.js

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ import express from 'express';
 
 const app = express();
 
+import path from 'path';
+
+
 app.use(express.static('public'));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve('public/frontpage/frontpage.html'));
+});
 
 const PORT = Number(process.env.PORT) || 8080;
 const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));

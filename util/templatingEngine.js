eebc56336281bf57845595c144f02e971560b54f
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-15 18:37:07 +0100

    created a footer.html and header.html to begin refactoring for SSR

---
+++ util/templatingEngine.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+export function readPage(filePath) {
+    return fs.readFileSync(filePath).toString;
+}
+
+const header = readPage(path.resolve("./public/components/header/header.html"));
+const footer = readPage(path.resolve)("./public/components/footer/footer.html");
+
+export function constructPage(pageContent, options = {}) {
+    let fullPage = header
+      .replace("$NAV_TITLE$", options.title || "My Site");
+      
+  
+    fullPage += pageContent;
+  
+    fullPage += footer;
+  
+    return fullPage;
+  }

c3b52c742161807d2396d7ecf2335aedef9f52d6
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-15 18:47:04 +0100

    deleted unused import and deleted the CSR solution

--- app.js
+++ app.js
@@ -1,44 +1,4 @@
-// import express from 'express';
-
-// const app = express();
-
-// import path from 'path';
-
-
-// app.use(express.static('public'));
-
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.resolve('public/frontpage/frontpage.html'));
-// });
-
-// app.get("/git", (req, res) => {
-//     res.sendFile(path.resolve('public/git/git.html'));
-// });
-
-// app.get("/nodejs", (req, res) => {
-//     res.sendFile(path.resolve('public/node.js/nodejs.html'))
-// });
-
-// app.get("/express", (req, res) => {
-//     res.sendFile(path.resolve('public/express/express.html'))
-// });
-
-// app.get("/javascript", (req, res) =>{
-//     res.sendFile(path.resolve('public/javascript/javascript.html'))
-// });
-
-// app.get("/restapi", (req, res) =>{
-//     res.sendFile(path.resolve('public/restapi/restapi.html'))
-// });
-
-
-
-// const PORT = Number(process.env.PORT) || 8080;
-// const server = app.listen(PORT, () => console.log("Server is running on port", server.address().port));
-
 import express from "express";
-import path from "path";
 import pagesRouter from "./router/pagesRouter.js";
 
 const app = express();

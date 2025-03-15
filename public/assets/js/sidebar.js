562925c0143a5bf5d5bdca7f65aad92a06e1c9b1
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-15 11:28:08 +0100

    Added custom styling to git.html and created styles.css

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -1,62 +1,61 @@
 document.addEventListener('DOMContentLoaded', function() {
   const sidebarContainer = document.getElementById('sidebar-container');
   sidebarContainer.innerHTML = `
-    <nav class=" top-0 w-48 h-full bg-blue-700 text-white transform -skew-y-6">
-      <div class="absolute inset-0 transform skew-y-6">
-        <ul class="py-8 space-y-4">
+    <nav class="w-64 h-screen bg-blue-900 text-white shadow-lg">
+      <div class="py-6 px-4">
+        <h2 class="text-xl font-bold mb-6">Documentation</h2>
+        <ul class="space-y-2">
           <li>
-            <a href="/" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+            <a href="/" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
               Home
             </a>
           </li>
           <li>
-            <a href="/git" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+            <a href="/git" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
               Git
             </a>
           </li>
           <li>
-            <a href="/nodejs" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+            <a href="/nodejs" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
               Node.js
             </a>
           </li>
           <li>
-            <a href="/express" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+            <a href="/express" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
               Express.js
             </a>
           </li>
           <li>
-            <a href="/restapi" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
+            <a href="/restapi" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
               Rest API
             </a>
           </li>
-          <li>
-            <div class="group">
-              <a href="/javascript" class="block px-4 py-2 hover:bg-blue-500 rounded transition">
-                JavaScript
-              </a>
-              <ul class="hidden group-hover:block bg-blue-700 mt-1 space-y-2 rounded">
-                <li>
-                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
-                    Datatypes in JavaScript
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
-                    Scopes in JavaScript
-                  </a>
-                </li>
-                <li>
-                  <a href="#" class="block px-4 py-2 hover:bg-blue-600 transition">
-                    Functions in JavaScript
-                  </a>
-                </li>
-                <li>
-                  <a href="/javascript/#loops" class="block px-4 py-2 hover:bg-blue-600 transition">
-                    Loops in JavaScript
-                  </a>
-                </li>
-              </ul>
-            </div>
+          <li class="relative group">
+            <a href="/javascript" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
+              JavaScript
+            </a>
+            <ul class="absolute left-full top-0 hidden group-hover:block bg-blue-900 shadow-xl mt-0 space-y-1 w-52">
+              <li>
+                <a href="#" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
+                  Datatypes
+                </a>
+              </li>
+              <li>
+                <a href="#" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
+                  Scopes
+                </a>
+              </li>
+              <li>
+                <a href="#" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
+                  Functions
+                </a>
+              </li>
+              <li>
+                <a href="/javascript/#loops" class="block px-3 py-2 rounded hover:bg-blue-800 transition">
+                  Loops
+                </a>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>

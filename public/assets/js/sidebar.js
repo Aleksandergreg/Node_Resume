bb4541e49556d6e19269f6c7b150f969adfb1307
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-16 11:19:17 +0100

    Feat: added the Date in JS to the sidebar for navigation

--- public/assets/js/sidebar.js
+++ public/assets/js/sidebar.js
@@ -71,6 +71,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     Loops
                   </a>
                 </li>
+                 <li>
+                  <a href="/javascript/#dates" class="block px-6 py-1.5 text-sm hover:bg-primary-50 transition text-secondary-700 hover:text-primary-700">
+                    Date
+                  </a>
+                </li>
               </ul>
             </li>
           </ul>

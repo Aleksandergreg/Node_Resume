d0aeaa9bb8671e67680701964ccc9851bee3ac75
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-16 18:36:21 +0100

    Bug: Buttons wasn't working as I forgot to assign them to anything

--- util/templatingEngine.js
+++ util/templatingEngine.js
@@ -19,7 +19,7 @@ export function constructPage(pageContent, options = {}) {
   let navigationButton = "";
 
   if(options.prevUrl || options.nextUrl) {
-   `
+    navigationButton = `
     <div class="flex justify-between p-4">
       ${options.prevUrl 
         ? `<a href="${options.prevUrl}" class="px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700">

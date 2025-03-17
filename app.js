eff97c35909706e0e4d982003462580b89add587
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-17 11:12:07 +0100

    Build: Tried editing app.js for vercel deployment

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-import express from 'express';
-import pagesRouter from './router/pagesRouter.js';
+// import express from 'express';
+// import pagesRouter from './router/pagesRouter.js';
+
+// const app = express();
+
+// app.use(express.static("public"));
+
+// app.use(pagesRouter);
+
+// const PORT = Number(process.env.PORT) || 8080;
+// app.listen(PORT, () => console.log(`Server is running on port ${ PORT }`));
+import express from "express";
+import pagesRouter from "./router/pagesRouter.js";
 
 const app = express();
 
 app.use(express.static("public"));
-
 app.use(pagesRouter);
 
-const PORT = Number(process.env.PORT) || 8080;
-app.listen(PORT, () => console.log(`Server is running on port ${ PORT }`));
+// Export the app for Vercel
+export default app;

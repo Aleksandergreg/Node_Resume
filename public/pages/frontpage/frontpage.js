90ad0a7d5613ae93482d4e0ada9eb531f42a4d6a
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-18 18:15:33 +0100

    Style: Linted

--- public/pages/frontpage/frontpage.js
+++ public/pages/frontpage/frontpage.js
@@ -1,34 +1,29 @@
- 
+function openModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.add("show");
+  document.body.style.overflow = "hidden";
+}
 
-  function openModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.classList.add('show');
-    document.body.style.overflow = 'hidden'; 
-  }
+function closeModal(modalId) {
+  const modal = document.getElementById(modalId);
+  modal.classList.remove("show");
+  document.body.style.overflow = "auto";
+}
 
-  function closeModal(modalId) {
-    const modal = document.getElementById(modalId);
-    modal.classList.remove('show');
-    document.body.style.overflow = 'auto'; 
-  }
-
-  window.onclick = function(event) {
-    const modals = document.getElementsByClassName('modal');
-    for (let i = 0; i < modals.length; i++) {
-      if (event.target === modals[i]) {
-        closeModal(modals[i].id);
-      }
+window.onclick = function (event) {
+  const modals = document.getElementsByClassName("modal");
+  for (let i = 0; i < modals.length; i++) {
+    if (event.target === modals[i]) {
+      closeModal(modals[i].id);
     }
   }
+};
 
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'Escape') {
-      const openModals = document.querySelectorAll('.modal.show');
-      openModals.forEach(modal => {
-        closeModal(modal.id);
-      });
-    }
-  });
-
-
-
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    const openModals = document.querySelectorAll(".modal.show");
+    openModals.forEach((modal) => {
+      closeModal(modal.id);
+    });
+  }
+});

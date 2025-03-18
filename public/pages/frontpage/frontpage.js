cd5acf062760e461e142860789f500dfc95a3d8e
Aleksandergreg <alek3691@stud.kea.dk>
2025-03-18 17:58:53 +0100

    Feat: Added styling and JS logic to modal. Going to implement on frontpage.html

---
+++ public/pages/frontpage/frontpage.js
@@ -0,0 +1,34 @@
+ 
+
+  function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+    document.body.style.overflow = 'hidden'; 
+  }
+
+  function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.remove('show');
+    document.body.style.overflow = 'auto'; 
+  }
+
+  window.onclick = function(event) {
+    const modals = document.getElementsByClassName('modal');
+    for (let i = 0; i < modals.length; i++) {
+      if (event.target === modals[i]) {
+        closeModal(modals[i].id);
+      }
+    }
+  }
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+      const openModals = document.querySelectorAll('.modal.show');
+      openModals.forEach(modal => {
+        closeModal(modal.id);
+      });
+    }
+  });
+
+
+
